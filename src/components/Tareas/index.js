3d8dc6f322635e57fc0e652aaa362d4ff9004715
Jose Hernandez (MacBookAir) <jghernandez84@gmail.com>
2019-07-04 01:58:58 -0400

    Iniciando componente de tareas

---
+++ src/components/Tareas/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Tareas extends Component {
+	constructor() {
+		super();
+		this.state = {}
+    }
+    
+	render() {
+		return (
+			<div>
+                <h1>Tareas</h1>
+			</div>
+		)
+	}
+};
+
+export default Tareas;
